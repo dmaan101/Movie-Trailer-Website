8c083616e126896df25785bd7304aade70ecd22c
Daksh Maan <daksh.maan@gmail.com>
2017-07-13 10:04:56 +0530

    Modify:add functions descriptions as comments

--- media.py
+++ media.py
@@ -1,10 +1,11 @@
 import webbrowser
 class Movie():
+    #function containes all the requiered information about the movie
     def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url   = poster_image
         self.trailer_youtube_url = trailer_youtube
-
+    #function used to play the trailer using youtube url
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
